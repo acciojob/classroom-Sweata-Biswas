3bc7dc397a6c65171386e544cfe85bf6c1158ec7
Sweata-Biswas <biswassweata22@gmail.com>
2022-12-30 13:56:00 +0530

    second commit

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -9,4 +9,34 @@ public class Student {
     public Student(){
 
     }
+
+    public Student(String name, int age, double averageScore) {
+        setName(name);
+        setAge(age);
+        setAverageScore(averageScore);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
 }
